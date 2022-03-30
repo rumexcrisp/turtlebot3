06e99067ff23337c2b82c33cda12f18ac5885792
Julian Baisch <julian.baisch@de.kaercher.com>
2022-03-30 20:16:41 +0200

    octomap changes

--- turtlebot3_slam/config/turtlebot3_lds_2d.lua
+++ turtlebot3_slam/config/turtlebot3_lds_2d.lua
@@ -30,7 +30,7 @@ options = {
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 1,
+  num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -38,17 +38,14 @@ options = {
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 0.1,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  imu_sampling_ratio = 0.1,
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 4.0
-TRAJECTORY_BUILDER_2D.max_z = 0.5
-TRAJECTORY_BUILDER_2D.min_z = 0
+TRAJECTORY_BUILDER_2D.max_range = 3.5
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
@@ -56,6 +53,5 @@ TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
-POSE_GRAPH.optimize_every_n_nodes = 0 -- 0 = deactivate global planner
 
 return options
