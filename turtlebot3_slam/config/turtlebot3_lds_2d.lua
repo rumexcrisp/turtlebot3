01c34bdd32835c583db02188da42be5be2ff6a08
Julian Baisch <julian.baisch@de.kaercher.com>
2022-04-14 17:42:12 +0200

    update cartographer

--- turtlebot3_slam/config/turtlebot3_lds_2d.lua
+++ turtlebot3_slam/config/turtlebot3_lds_2d.lua
@@ -24,7 +24,7 @@ options = {
   odom_frame = "odom",
   provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -43,6 +43,7 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
+MAP_BUILDER.num_background_threads = 12
 
 TRAJECTORY_BUILDER_2D.min_range = 0.1
 TRAJECTORY_BUILDER_2D.max_range = 10.0
@@ -51,6 +52,11 @@ TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 2e2
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 4e2
+
+POSE_GRAPH.optimize_every_n_nodes = 320
+POSE_GRAPH.global_sampling_ratio = 1
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
